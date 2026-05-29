96e465feda17481ea7bcaf9c06dfecc1aa47ddc4
Dikshan kc <146184803+Dikshan23@users.noreply.github.com>
2026-05-29 21:37:35 +0545

    Update sitemap URL to new domain

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://www.genuinesuppliers.com/sitemap.xml",
+    sitemap: "https://www.genuinesuppliers.com.np/sitemap.xml",
   };
 }
